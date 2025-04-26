6c2276b41d878516cb041398074951697410d122
Abdumalik <abdumalikabduvohobov341@gmail.com>
2025-04-26 09:59:57 +0300

    finished

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,14 @@
 import asyncio
 from os import getenv
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
 from dotenv import load_dotenv
 from handlers import router as handlers_router
 
 load_dotenv()
+
+session = AiohttpSession(proxy="http://proxy.server:3128")
+
 
 TOKEN = getenv("BOT_TOKEN")
 
@@ -14,7 +18,7 @@ dp.include_router(handlers_router)
 
 # Run the bot
 async def main() -> None:
-    bot = Bot(token=TOKEN)
+    bot = Bot(token=TOKEN, session = session)
     await dp.start_polling(bot)
 
 
